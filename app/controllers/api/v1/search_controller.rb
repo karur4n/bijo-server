665f1a79910c8929973c7ba07d32e4e1437e3c88
karur4n <karuran.f@gmail.com>
2015-02-24 08:22:11 +0900

    client の管理を concerns から lib に移動する
    conerns についてあんまり分かってないのに使ってた

--- app/controllers/api/v1/search_controller.rb
+++ app/controllers/api/v1/search_controller.rb
@@ -1,11 +1,10 @@
 class Api::V1::SearchController < ApplicationController
-  include TumblrClient
-
   before_action :has_valid_query?
 
   def index
     if has_valid_query?
-      response = get_with_tagg(name_query)
+      response = tumblr_client.get_with_tagg(name_query)
+      redis_client.add_name(name_query)
       render json: response
     else
       head :bad_request
@@ -20,5 +19,13 @@ class Api::V1::SearchController < ApplicationController
 
   def has_valid_query?
     !!name_query && name_query.present?
+  end
+
+  def tumblr_client
+    @tumblr_client ||= TumblrClient.new
+  end
+
+  def redis_client
+    @redis_client ||= RedisClient.new
   end
 end
---
+++ lib/tumblr_client.rb
@@ -0,0 +1,34 @@
+class TumblrClient
+  def initialize
+    @client = Tumblr::Client.new
+
+    Tumblr.configure do |config|
+      config.consumer_key = Figaro.env.consumer_key!
+      config.consumer_secret = Figaro.env.consumer_secret!
+      config.oauth_token = Figaro.env.oauth_token!
+      config.oauth_token_secret = Figaro.env.oauth_token_secret!
+    end
+  end
+
+  def get_with_tagg(query)
+    results = @client.tagged(query)
+
+    urls = parse_search_results(results)
+    urls.to_json
+  end
+
+  def parse_search_results(results)
+    urls = []
+
+    results.each do |res|
+      next unless res['type'] == 'photo'
+
+      photos = res['photos']
+      photos.each do |photo|
+        urls <<  photo['original_size']['url']
+      end
+    end
+
+    urls
+  end
+end
