f108fdf2187c8a8b7c38dce5ee4552e3760bb1c0
karur4n <karuran.f@gmail.com>
2015-02-25 10:47:30 +0900

    今のところ RedisClient を何も使ってないので空にしておく

--- lib/redis_client.rb
+++ lib/redis_client.rb
@@ -1,10 +1,2 @@
 class RedisClient
-  def initialize
-    @name_set ||= Redis::Set.new('bijo_name')
-  end
-
-  def add_name(name)
-    @name_set << name
-    p "#{@name_set} ------------------ this is reddddddddddddddddddddddddddddddddiiiiiiiiiiiiiiiiiiiiiiis"
-  end
 end
