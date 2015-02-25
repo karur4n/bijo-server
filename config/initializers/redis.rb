50fe322eccc2413f405c9e394b19b7d21245f27c
karur4n <karuran.f@gmail.com>
2015-02-25 14:11:51 +0900

    redis の initializer に production の設定を追加する

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,1 +1,5 @@
-Redis.current = Redis.new(host: '127.0.0.1', port: 6379)
+if Rails.env == 'develop'
+  Redis.current = Redis.new(host: '127.0.0.1', port: 6379)
+elsif Rails.env == 'production'
+  Redis.current = Redis.new(:url => ENV["REDISCLOUD_URL"])
+end
